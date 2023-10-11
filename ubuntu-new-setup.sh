608a0771a57e69d2464be7fdd1d67f1f37094991
Dmitry <45873005+bokhanych@users.noreply.github.com>
2023-10-11 17:28:25 +0300

    Create ubuntu-new-setup.sh

---
+++ ubuntu-new-setup.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+# Developed by bokhanych [https://www.linkedin.com/in/bokhanych/]
+# Tested on Ubuntu 22.04.3 LTS
+
+# REQUEST INFO
+echo -n "ENTER SERVER FQDN [Example: example.server.com]: "; read NEWHOSTNAME
+echo -n "GENERATE SSH PORT? [Y/<Enter> to default '22']: "; read SSH_PORT
+echo -n "INSTALL ZABBIX-AGENT? [Y/<Enter> to default 'NO']: "; read ZABBIX_AGENT
+
+# VARIABLES
+USER="YOUR_USER"
+PASSWORD='YOUR_PASSWORD'
+INET_IFACE=$(ip route get 8.8.8.8 | awk '/dev/ {f=NR} f&&NR-1==f' RS=" ")
+SERVER_IP=$(hostname  -I | cut -f1 -d' ')
+if [ "$SSH_PORT" = "" ]
+then
+    SSH_PORT="22"
+else
+    SSH_PORT=$(shuf -i 30000-40000 -n 1)
+fi
+
+# CHECK-IN
+clear
+echo "hostname: $NEWHOSTNAME"
+echo "ip: $SERVER_IP"
+echo "port: $SSH_PORT"
+echo "user: $USER"
+echo "password: $PASSWORD"
+echo "network interface: $INET_IFACE"
+echo "zabbix-agent: $ZABBIX_AGENT"
+sleep 5
+
+# HOSTNAME, DATE and HISTORY FORMAT
+echo 127.0.0.1 localhost > /etc/hosts
+echo 127.0.1.1 $NEWHOSTNAME >> /etc/hosts
+echo $NEWHOSTNAME > /etc/hostname
+timedatectl set-timezone Europe/Moscow
+echo 'HISTTIMEFORMAT="%F "' >> ~/.bashrc
+
+# UPDATE & SOFT
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+apt-get -y update
+DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt install ssh openssl htop iptables-persistent net-tools unattended-upgrades apt-listchanges -y -q
+dpkg-reconfigure -f noninteractive unattended-upgrades
+
+# USER
+adduser $USER --gecos "" --disabled-password
+usermod -aG sudo $USER
+echo $USER:$PASSWORD|chpasswd
+
+# PAM SECURITY
+cp /etc/pam.d/common-password /etc/pam.d/common-password.bakapa
+apt install libpam-pwquality -y -q
+cat << EOF > /etc/pam.d/common-password
+password        requisite                       pam_pwquality.so retry=10 minlen=12 ucredit=-1 lcredit=-1 dcredit=-1 ocredit=-1
+password        [success=1 default=ignore]      pam_unix.so obscure use_authtok try_first_pass yescrypt
+password        requisite                       pam_deny.so
+password        required                        pam_permit.so
+EOF
+cp /etc/pam.d/common-auth /etc/pam.d/common-auth.bakapa
+apt install libpwquality-tools -y -q
+cat << EOF > /etc/pam.d/common-auth
+auth    required                        pam_faillock.so preauth audit
+auth    [success=1 default=ignore]      pam_unix.so
+auth    [default=die]                   pam_faillock.so authfail audit deny=6 unlock_time=600
+auth    sufficient                      pam_faillock.so authsucc audit deny=6 unlock_time=600
+auth    requisite                       pam_deny.so
+auth    required                        pam_permit.so
+auth    optional                        pam_cap.so
+EOF
+
+# FAIL2BAN
+apt install fail2ban -y -q
+cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.conf.bakapa
+sed -i "s%port    = ssh%port    = $SSH_PORT%g" /etc/fail2ban/jail.conf
+systemctl enable fail2ban
+
+# IPTABLES
+cp /etc/iptables/rules.v4 /etc/iptables/rules.v4.bakapa
+cp /etc/iptables/rules.v6 /etc/iptables/rules.v6.bakapa
+cat << EOF > /etc/iptables/rules.v4
+*mangle
+:PREROUTING ACCEPT [915:64761]
+:INPUT ACCEPT [910:63925]
+:FORWARD ACCEPT [0:0]
+:OUTPUT ACCEPT [797:116043]
+:POSTROUTING ACCEPT [797:116043]
+COMMIT
+*filter
+:INPUT DROP [37:3819]
+:FORWARD DROP [0:0]
+:OUTPUT ACCEPT [637:103243]
+-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+-A INPUT -i lo -j ACCEPT
+-A INPUT -p tcp -m tcp --dport SSH_PORT -j ACCEPT
+-A INPUT -p icmp -m limit --limit 1/sec -j ACCEPT
+#-A INPUT -p tcp --dport 10050 -j ACCEPT
+-A INPUT -m state --state INVALID -j DROP
+COMMIT
+*nat
+:PREROUTING ACCEPT [43:4707]
+:INPUT ACCEPT [1:52]
+:OUTPUT ACCEPT [28:1913]
+:POSTROUTING ACCEPT [28:1913]
+COMMIT
+EOF
+cat << EOF > /etc/iptables/rules.v6
+*filter
+:INPUT DROP [0:0]
+:FORWARD DROP [0:0]
+:OUTPUT ACCEPT [0:0]
+COMMIT
+EOF
+sed -i "s%eth0%$INET_IFACE%g" /etc/iptables/rules.v4
+sed -i "s%SSH_PORT%$SSH_PORT%g" /etc/iptables/rules.v4
+
+# SSHD
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bakapa
+cat << EOF > /etc/ssh/sshd_config
+Port SSH_PORT
+HostKey /etc/ssh/ssh_host_ed25519_key
+HostKey /etc/ssh/ssh_host_rsa_key
+HostKey /etc/ssh/ssh_host_ecdsa_key
+KexAlgorithms curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256
+Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr
+MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com
+LoginGraceTime 20
+MaxAuthTries 3
+IgnoreRhosts yes
+HostbasedAuthentication no
+PermitEmptyPasswords no
+ChallengeResponseAuthentication no
+KerberosAuthentication no
+GSSAPIAuthentication no
+X11Forwarding no
+X11DisplayOffset 10
+PrintMotd no
+PrintLastLog no
+TCPKeepAlive yes
+PermitUserEnvironment no
+AllowAgentForwarding no
+AllowTcpForwarding no
+PermitTunnel no
+Subsystem sftp /usr/lib/openssh/sftp-server
+Banner yes
+UsePAM yes
+UseDNS no
+ClientAliveInterval 600
+ClientAliveCountMax 3
+Match User root
+PasswordAuthentication no
+EOF
+sed -i "s%SSH_PORT%$SSH_PORT%g" /etc/ssh/sshd_config
+sed -i "s%port    = ssh%port    = $SSH_PORT%g" /etc/fail2ban/jail.conf
+
+# ZABBIX AGENT INSTALL
+if [ "$ZABBIX_AGENT" = "Y" ]
+then
+    UBUNTU_VERSION=$(awk -F '=' '/DISTRIB_RELEASE/ {print $2}' /etc/lsb-release)
+    UBUNTU_VERSION="$UBUNTU_VERSION"_all.deb
+    wget https://repo.zabbix.com/zabbix/6.5/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.5-1+ubuntu$UBUNTU_VERSION
+    dpkg -i zabbix-release_*.deb
+    apt update -y && apt install zabbix-agent2 && systemctl enable zabbix-agent2
+    rm -r zabbix-release_*.deb
+    openssl rand -hex 32 | tee -a /etc/zabbix/zabbix_agent2.psk;
+    chmod 400 /etc/zabbix/zabbix_agent2.psk;
+    chown zabbix:zabbix /etc/zabbix/zabbix_agent2.psk
+cat << EOF > /etc/zabbix/zabbix_agent2.conf
+PidFile=/var/run/zabbix/zabbix_agent2.pid
+LogFile=/var/log/zabbix/zabbix_agent2.log
+Server=YOUR_SERVER_IP
+ServerActive=YOUR_SERVER_IP:10051
+Hostname=HOSTNAME
+Include=/etc/zabbix/zabbix_agent2.d/*.conf
+Timeout=30
+#AllowKey=system.run[*]
+#TLSConnect=psk
+#TLSAccept=psk
+#TLSPSKIdentity=HOSTNAME
+#TLSPSKFile=/etc/zabbix/zabbix_agent2.psk
+EOF
+    sed -i "s%HOSTNAME%$NEWHOSTNAME%g" /etc/zabbix/zabbix_agent2.conf
+    sed -i "s%#-A INPUT -p tcp --dport 10050 -j ACCEPT%-A INPUT -p tcp --dport 10050 -j ACCEPT%g" /etc/iptables/rules.v4
+fi
+
+# REGENERATE THE CLIENT ID (for cloned machine)
+rm -f /etc/machine-id && dbus-uuidgen --ensure=/etc/machine-id
+
+# UPGRADE & REBOOT
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" dist-upgrade -y
+apt autoremove -y
+shutdown -r now
+
+
+# NOTES:
+# UNLOCK USER "test":
+# faillock --user test && faillock --user test --reset
+# RELOAD IPTABLES:
+# /etc/init.d/netfilter-persistent reload && systemctl restart fail2ban && /etc/init.d/netfilter-persistent status
